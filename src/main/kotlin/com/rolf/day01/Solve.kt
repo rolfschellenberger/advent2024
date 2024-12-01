5f1cc124a46f4d42be0e06f51991ae322c177258
Rolf <rolf.schellenberger@fastnedcharging.com>
2024-12-01 06:16:59 +0100

    Day 1

--- src/main/kotlin/com/rolf/day01/Solve.kt
+++ src/main/kotlin/com/rolf/day01/Solve.kt
@@ -1,6 +1,8 @@
 package com.rolf.day01
 
 import com.rolf.Day
+import com.rolf.util.splitLine
+import kotlin.math.absoluteValue
 
 fun main() {
     Solve().run()
@@ -8,8 +10,36 @@ fun main() {
 
 class Solve : Day() {
     override fun solve1(lines: List<String>) {
+        val left = leftList(lines).sorted()
+        val right = rightList(lines).sorted()
+
+        println(
+            left.zip(right).sumOf { (l, r) ->
+                (l - r).absoluteValue
+            }
+        )
     }
 
     override fun solve2(lines: List<String>) {
+        val left = leftList(lines)
+        val right = rightList(lines)
+
+        println(
+            left.sumOf { l ->
+                l * right.count { it == l }
+            }
+        )
+    }
+
+    fun leftList(lines: List<String>): List<Int> {
+        return lines.map { line ->
+            splitLine(line, " ").first().toInt()
+        }
+    }
+
+    fun rightList(lines: List<String>): List<Int> {
+        return lines.map { line ->
+            splitLine(line, " ").last().toInt()
+        }
     }
 }
