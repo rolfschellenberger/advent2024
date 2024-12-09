fb436928339e8809b6433a7f6b753805957eb1be
Rolf <rolf.schellenberger@fastnedcharging.com>
2024-12-09 09:16:17 +0100

    Day 9

--- src/main/kotlin/com/rolf/day09/Day09.kt
+++ src/main/kotlin/com/rolf/day09/Day09.kt
@@ -10,49 +10,48 @@ fun main() {
 class Solve : Day() {
     override fun solve1(lines: List<String>) {
         val numbers = lines.map { splitLine(it).map { it.toInt() } }.flatten()
-//        println(numbers)
-
-        val array = IntArray(numbers.sum()) { -1 }
-//        println(array.toList())
-        var file = true
-        var pos = 0
-        var fileId = 0
-        for (number in numbers) {
-            for (i in 0 until number) {
-//                println("number: $number, fileId: $fileId, file: $file, pos: $pos")
-                if (file) {
-                    array[pos] = fileId
-                }
-                pos++
-            }
-            if (file) {
-                fileId++
-            }
-            file = !file
-//            println()
-        }
-//        println(array.toList())
+        val array = toArray(numbers)
 
         // Move the right most non-zero number to the first -1 position in the array
         // Repeat until there are only -1's at the end of the array
         while (!sorted(array)) {
-//            println(array.toList())
-
             val lastPositiveNumber = array.indexOfLast { it >= 0 }
             val firstNegativeNumber = array.indexOfFirst { it < 0 }
             array[firstNegativeNumber] = array[lastPositiveNumber]
             array[lastPositiveNumber] = -1
         }
 
-//        println(array.toList().joinToString(""))
-
-        var sum = 0L
-        for ((index, number) in array.withIndex()) {
-            if (number > 0) {
-                sum += index * number
+        println(
+            array.withIndex().sumOf { (index, number) ->
+                if (number > 0) {
+                    index * number.toLong()
+                } else {
+                    0
+                }
             }
+        )
+    }
+
+    private fun toArray(numbers: List<Int>): IntArray {
+        val array = IntArray(numbers.sum()) { -1 }
+        var file = true
+        var pos = 0
+        var fileId = 0
+        for (number in numbers) {
+            // Create N elements in the array
+            for (i in 0 until number) {
+                if (file) {
+                    array[pos] = fileId
+                }
+                pos++
+            }
+            // Increment file id when moving to the next file
+            if (file) {
+                fileId++
+            }
+            file = !file
         }
-        println(sum)
+        return array
     }
 
     private fun sorted(array: IntArray): Boolean {
@@ -63,18 +62,13 @@ class Solve : Day() {
 
     override fun solve2(lines: List<String>) {
         val numbers = lines.map { splitLine(it).map { it.toInt() } }.flatten()
-//        println(numbers)
         val chunks = toChunks(numbers).toMutableList()
-//        chunks.forEach { println(it) }
 
         // From right to left: try to move each chunk to the left most available free space
         for (i in chunks.indices.reversed()) {
-//            println(i)
-//            println(chunks[i])
             val chunk = chunks[i]
             if (chunk.isFile) {
                 val firstEmptySpot = firstEmptySpot(chunks.subList(0, i), chunk)
-//                println(firstEmptySpot)
                 if (firstEmptySpot >= 0) {
                     // Reduce empty spot
                     chunks[firstEmptySpot] =
@@ -83,15 +77,9 @@ class Solve : Day() {
                     chunks.removeAt(i)
                     chunks.add(firstEmptySpot, chunk)
                     chunks.add(i, Chunk(chunk.size, false, chunk.fileId))
-//                    chunks[firstEmptySpot] = chunk
-//                    chunks[i] = chunk.copy(isFile = false)
                 }
             }
-//            println(chunks)
-//            printChunks(chunks)
-//            println()
         }
-//        printChunks(chunks)
 
         var sum = 0L
         var index = 0
@@ -100,46 +88,11 @@ class Solve : Day() {
                 for (i in 0 until chunk.size) {
                     val chunkIndex = index + i
                     sum += chunkIndex * chunk.fileId
-//                    println("index: ${index + i}")
-//                    println(chunk.fileId)
                 }
-//                println(chunk)
-//                sum += index * chunk.fileId
             }
             index += chunk.size
         }
         println(sum)
-    }
-
-    private fun printChunks(chunks: MutableList<Chunk>) {
-        val builder = StringBuilder()
-
-        for (chunk in chunks) {
-            if (chunk.isFile) {
-                for (i in 0 until chunk.size) {
-                    builder.append(chunk.fileId)
-                }
-            } else {
-                for (i in 0 until chunk.size) {
-                    builder.append(".")
-                }
-            }
-        }
-
-        println(builder.toString())
-    }
-
-    private fun firstEmptySpot(
-        chunks: MutableList<Chunk>,
-        chunk: Chunk,
-    ): Int {
-//        println(chunks)
-        for ((index, space) in chunks.withIndex()) {
-            if (!space.isFile && space.size >= chunk.size) {
-                return index
-            }
-        }
-        return -1
     }
 
     private fun toChunks(numbers: List<Int>): List<Chunk> {
@@ -156,19 +109,15 @@ class Solve : Day() {
         return chunks
     }
 
-//    private fun firstEmptySpot(array: MutableList<Chunk>, minLength: Int): Int {
-//        // Find a minimum number of -1's in a row in the array
-//        var length = 0
-//        for ((index, number) in array.withIndex()) {
-//            if (number == -1) {
-//                length++
-//            } else {
-//                length = 0
-//            }
-//            if (length >= minLength) {
-//                return index - length
-//            }
-//        }
-//        return -1
-//    }
+    private fun firstEmptySpot(
+        chunks: MutableList<Chunk>,
+        chunk: Chunk,
+    ): Int {
+        for ((index, space) in chunks.withIndex()) {
+            if (!space.isFile && space.size >= chunk.size) {
+                return index
+            }
+        }
+        return -1
+    }
 }
