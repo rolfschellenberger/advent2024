431418c6cc967921a386f7c43c942b96604fb3e4
Rolf <rolf.schellenberger@fastnedcharging.com>
2024-12-23 06:29:41 +0100

    Day 23

---
+++ src/main/kotlin/com/rolf/day23/Day23.kt
@@ -0,0 +1,50 @@
+package com.rolf.day23
+
+import com.rolf.Day
+import com.rolf.util.EdgeType
+import com.rolf.util.Graph
+import com.rolf.util.Vertex
+
+fun main() {
+    Solve().run()
+}
+
+class Solve : Day() {
+    override fun realRun(): Boolean {
+        return true
+    }
+
+    override fun solve1(lines: List<String>) {
+        val network = parseNetwork(lines)
+        val triads = mutableSetOf<Set<String>>()
+        for (a in network.vertices()) {
+            for (b in network.neighbours(a.id)) {
+                for (c in network.neighbours(b)) {
+                    if (network.neighbours(c).contains(a.id)) {
+                        triads.add(setOf(a.id, b, c))
+                    }
+                }
+            }
+        }
+        println(
+            triads.count { it.any { it.startsWith("t") } }
+        )
+    }
+
+    override fun solve2(lines: List<String>) {
+        val network = parseNetwork(lines)
+        val clique = network.largestCliques().first().sorted()
+        println(clique.joinToString(","))
+    }
+
+    private fun parseNetwork(lines: List<String>): Graph<String> {
+        val network = Graph<String>()
+        for (connection in lines) {
+            val (a, b) = connection.split("-")
+            network.addVertex(Vertex(a))
+            network.addVertex(Vertex(b))
+            network.addEdge(a, b, EdgeType.UNDIRECTED)
+        }
+        return network
+    }
+}
